bdef2cae1024d68ad4986678fb2813666fbadc29
Frannerz <francesstephens@hotmail.com>
2024-04-11 08:24:17 +0100

    error handling added for index route

--- virtual-environment/app.py
+++ virtual-environment/app.py
@@ -3,6 +3,7 @@ from flask_mysqldb import MySQL
 from config import mysql_settings as mss
 from forms import PetForm
 from utils import format_appointments
+from datetime import date
 
 app = Flask(__name__)
 
@@ -17,23 +18,31 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    # Pretend today is 15/4/24 (in real app this would be auto generated)
-    today = '2024-04-15'
+    today = date.today()
+    print(today)
     query = '''SELECT a.date, a.time AS 'Time', a.appointment_status AS 'Appointment Status', p.petname AS 'Pets Name', CONCAT(o.firstname, ' ', o.lastname) AS "Owner's name", o.phone AS 'Phone Number' 
     FROM Appointments a JOIN pets p ON a.petid = p.petid
     JOIN owners o ON o.ownerid = p.ownerid
-    WHERE date = %s;'''
+    WHERE date = %s
+    ORDER BY a.time;'''
+    try:
+        # Query the db
+        cursor = mysql.connection.cursor()
+        cursor.execute(query, (today,))
+        appointments = cursor.fetchall()
+        cursor.close()
+        # use imported utils function to edit the format of appointments
+        todays_appointments = format_appointments(appointments)
+        return render_template("index.html", todays_appointments=todays_appointments)
+    except:
+        print("Failed to read data from DB")
+        message = 'Sorry, could not retrieve appointments. Please try again later.'
+        return render_template("index.html", message=message, todays_appointments=[])
+    finally:
+        if cursor:
+            cursor.close()
+            print('The database is now closed')
 
-    # Query the db
-    cursor = mysql.connection.cursor()
-    cursor.execute(query, (today,))
-    appointments = cursor.fetchall()
-    cursor.close()
-
-    # use imported utils function to edit the format of appointments
-    todays_appointments = format_appointments(appointments)
-
-    return render_template("index.html", todays_appointments=todays_appointments)
 
 @app.route('/search')
 def search():
@@ -53,12 +62,14 @@ def cancel():
 
 @app.route('/add_patient', methods=['GET', 'POST'])
 def add_patient():
+    # change this to owner name/email on form?
     # Query for getting pets list:
     pet_query = '''SELECT p.petid, p.petname, p.species, p.age,CONCAT(o.firstname, ' ', o.lastname) AS "Owner's name", MAX(a.notes) AS notes
                     FROM pets p 
                     JOIN owners o ON p.ownerid = o.ownerid
                     LEFT JOIN appointments a ON p.petid = a.petid
-                    GROUP BY p.petid;'''
+                    GROUP BY p.petid
+                    ORDER BY petid;'''
     # interact with database to get existing pets list
     cursor = mysql.connection.cursor()
     cursor.execute(pet_query)

8aa19d611350ac76ed65d998311635675fa6ca45
Frannerz <francesstephens@hotmail.com>
2024-04-13 09:22:52 +0100

    fixed some bugs, db_query not working

--- virtual-environment/db_utils.py
+++ virtual-environment/db_utils.py
@@ -1,9 +1,9 @@
-from datetime import timedelta, datetime
+from datetime import datetime
 import mysql.connector
 from config import mysql_settings as mss
 from datetime import date
 
-
+# Function to create the connection with the db
 def _connect_to_db():
     try:
         cnx = mysql.connector.connect(
@@ -18,40 +18,37 @@ def _connect_to_db():
     except mysql.Error as e:
         raise Exception(f"Failed to connect to the database: {e}")
 
-
+# Function to format the date and time for display in appointments
 def format_appointments(results):
     todays_appointments = []
     for row in results:
-        # Format the date to format '%Y-%m-%d'
         formatted_date = row[0].strftime('%Y-%m-%d')
-        # Format time to format '%H:%M'
         formatted_time = (datetime.min + row[1]).strftime('%H:%M')
-        # Creating new tuple for the row with the formatted time:
         new_row = (formatted_date, formatted_time) + row[2:]
-        # append to list
         todays_appointments.append(new_row)
     return todays_appointments
 
-def query_db(query, fetch=True):
-    result = []
-    try:
-        db_connection = _connect_to_db()
-        cur = db_connection.cursor()
-        print("Connected to DB")
-        cur.execute(query)
-        db_connection.commit()
-        if fetch:
-            result = cur.fetchall()
-    except Exception as e:
-        print(f"Failed to execute query: {e}")
-        return None
-    finally:
-        if db_connection:
-            db_connection.close()
-            print("DB connection is closed")
-    return result
+# Function to query the db- not working properly atm!!!
+# def query_db(query, fetch = True):
+#     result = []
+#     try:
+#         db_connection = _connect_to_db()
+#         cur = db_connection.cursor()
+#         print("Connected to DB")
+#         cur.execute(query)
+#         db_connection.commit()
+#         if fetch:
+#             cur.fetchall()
+#     except Exception as e:
+#         print(f"Failed to execute query: {e}")
+#         return None
+#     finally:
+#         if db_connection:
+#             db_connection.close()
+#             print("DB connection is closed")
+#     return result
 
-
+# Function to query db and get appointments for today ('/')
 def get_todays_appointments():
     today = date.today()
     todays_appointments = []
@@ -61,15 +58,22 @@ def get_todays_appointments():
     WHERE date = '{today}'
     ORDER BY a.time;'''
     try:
-        result = query_db(query)
-        if result:
-            todays_appointments = format_appointments(result)
+        db_connection = _connect_to_db()
+        cur = db_connection.cursor()
+        print("Connected to DB")
+        cur.execute(query)
+        result = cur.fetchall()
+        todays_appointments = format_appointments(result)
+        cur.close()
     except Exception as e:
         print(f"Failed to get today's appointments: {e}")
-
+    finally:
+        if db_connection:
+            db_connection.close()
+            print('DB connection closed')
     return todays_appointments
 
-
+# Function for viewing existing pets ('/patients')
 def get_all_patient_info():
     pet_query = '''SELECT p.petid, p.petname, p.species, p.age,CONCAT(o.firstname, ' ', o.lastname) AS "Owner's name", MAX(a.notes) AS notes
                     FROM pets p
@@ -79,23 +83,44 @@ def get_all_patient_info():
                     ORDER BY petid;'''
     # interact with database to get existing pets list
     try:
-        pets = query_db(pet_query)
-        return pets
+        db_connection = _connect_to_db()
+        cur = db_connection.cursor()
+        print("Connected to DB")
+        cur.execute(pet_query)
+        results = cur.fetchall()
+        cur.close()
+        return results
+        # pets = query_db(pet_query)
+        # return pets
     except Exception as e:
         print(f"Failed to get patient info: {e}")
         return []
+    finally:
+        if db_connection:
+            db_connection.close()
+            print('DB connection closed')
     
 
-
+# Function to add new pet to database (/patients/add)
 def add_patient_to_db(OwnerId, petName, species, age):
         new_pet_query = f'''INSERT INTO Pets (OwnerID, PetName, Species, Age) VALUES ({OwnerId}, '{petName}', '{species}', {age})'''
-        # Get new pets list from database (need to edit this further)
         try:
-            query_db(new_pet_query)
+            db_connection = _connect_to_db()
+            cur = db_connection.cursor()
+            print("Connected to DB")
+            cur.execute(new_pet_query)
+            db_connection.commit()
             print(f'{petName} successfully add to database!')
+            cur.close()
+            # query_db(new_pet_query, fetch=False)
+            
             return True
         except Exception as e:
             print(f"Failed to add patient info: {e}")
             return False
+        finally:
+            if db_connection:
+                db_connection.close()
+                print('DB connection closed')
        
    
--- virtual-environment/main.py
+++ virtual-environment/main.py
@@ -9,8 +9,8 @@ def generate_todays_appointments():
     except requests.exceptions.RequestException as e:
         print(f"Error fetching appointments: {e}")
         return None
-    except ValueError as e:
-        print(f"Error decoding JSON: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
         return None
 
 def search_by_date():
@@ -32,6 +32,32 @@ def add_new_patient(owner_id, name, species, age):
 
     return result.json()
 
+def view_pet_info():
+    try:
+        response = requests.get('http://127.0.0.1:3000/patients')
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching appointments: {e}")
+        return None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+# basic display for now, need to improve this!
+def display_info(data):
+    for item in data:
+        print(item, '\n')
+
+# Form for getting info about pets to add onto db
+def get_pet_info():
+    owner = input("Enter the owner id (1-10): ")
+    name = input("Enter the animal's name: ")
+    species = input("Enter the species of animal: ")
+    age = input("Enter the animal's age: ")
+    return [owner, name, species, age]
+
+
 def add_new_booking():
     pass
 
@@ -44,32 +70,24 @@ def alter_booking():
 def get_owner_info():
     pass
 
-# basic display for now, need to improve this!
-def display_appointments(data):
-    for item in data:
-        print(item, '\n')
-
-def get_pet_info():
-    owner = input("Enter the owner id (1-10): ")
-    name = input("Enter the animal's name: ")
-    species = input("Enter the species of animal: ")
-    age = input("Enter the animal's age: ")
-    return [owner, name, species, age]
 
 def run():
     print('*******************************************************')
     print('Hello, welcome to the vetinary practice booking system!')
     print('*******************************************************')
     print()
-    print("************ Today's Appointments ************")
-    display_appointments(generate_todays_appointments())
+    print("**************** Today's Appointments ****************")
+    display_info(generate_todays_appointments())
     action = input('''What action would you like to take? 
                    \n-To book new appointment, enter 'book'
-                   \n-To add new patient, enter 'add' ''')
+                   \n-To add new patient, enter 'add' 
+                   \n-To view existing patients, enter 'view' ''')
     # add more choices to action input
     if action == 'add':
         new_pet = get_pet_info()
         add_new_patient(new_pet[0], new_pet[1], new_pet[2], new_pet[3])
+    elif action == 'view':
+        display_info(view_pet_info())
 
     # add what to do if something else chosen
 

e5102f65de8c2a341e6a5a9d6b3509117c2d9532
norah <git@fatimasial.uk>
2024-04-15 11:13:36 +0100

    Config file to connect to database

---
+++ virtual-environment/config.py
@@ -0,0 +1,8 @@
+#Please change these settings according to your MySQL setup
+mysql_settings = {
+    "host": "localhost",
+    "user": "root",
+    #Please change password to the password for your MySQL account
+    "password": "password",
+    "db": "VetSurgery",
+}
